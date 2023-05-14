46f862d20d424c1eddc9c84cfff40e6ab8737ef6
Lucia Nasti <nastilucia@gmail.com>
2023-05-14 16:21:48 +0200

    First commit.

---
+++ src/oggetti/GestioneDate.java
@@ -0,0 +1,34 @@
+/*package oggetti;
+import java.util.*; 
+public class GestioneDate {
+AnnuncioRialzo nuovo; 
+public String giorno;
+public String mese; 
+public String anno; 
+public String dataSistema;
+
+public String dataPubblicazione; 
+public String dataScadenza; 
+
+
+
+public GestioneDate(){
+	this.dataSistema=  ""+ this.giorno+ "-" + this.mese + "-"+ this.anno; 
+	
+	
+}
+
+public String getGiorno(){
+	return this.giorno= Integer.toString(Calendar.DAY_OF_MONTH); 
+	
+}
+
+public String getMese(){
+	return this.mese=Integer.toString((Calendar.MONTH)+1);
+}
+public String getAnno(){
+	return this.anno=Integer.toString(Calendar.YEAR);
+}
+
+
+}*/
